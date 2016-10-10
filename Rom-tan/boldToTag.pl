961622185dd065b43c1fc9e2f3f092ad8c8e9fd8
kurema <kurema_makoto_software@yahoo.co.jp>
2016-10-10 13:11:25 +0900

    Romtan tool + Binary

---
+++ Rom-tan/boldToTag.pl
@@ -0,0 +1,26 @@
+﻿use utf8;
+binmode(STDOUT, ":utf8");
+binmode(STDIN, ":utf8");
+
+while(my $t=<STDIN>){
+if($t=~ /^<bold>(.+)<\/bold>\s?(.+)$/){
+print "<dt>" . basicEscape($1) . "</dt>\n";
+my @keys=split(/\s/,$1);
+if(@keys>1){
+foreach my $item (@keys){
+print "<key type=\"表記\">" . basicEscape($item) . "</key>\n";
+}
+}
+print "<dd>" . basicEscape($2) . "</dd>\n";
+}
+}
+
+sub basicEscape{
+my $t=$_[0];
+$t=~ s/&/&amp;/;
+$t=~ s/</&lt;/;
+$t=~ s/>/&gt;/;
+$t=~ s/"/&quot;/;
+$t=~ s/'/&apos;/;
+return $t;
+}
---
+++ Rom-tan/fgToTag.pl
@@ -0,0 +1,19 @@
+﻿use utf8;
+binmode(STDOUT, ":utf8");
+binmode(STDIN, ":utf8");
+
+while(my $t=<STDIN>){
+if($t=~ /^(([^［］]+)［([^［］]+)］)(.+)$/){
+print "<dt>" . basicEscape($1) . "</dt>\n" . "<key type=\"表記\">" . basicEscape($2) . "</key>\n" . "<key type=\"表記\">" . basicEscape($3) . "</key>\n" . "<dd>" . basicEscape($4) . "</dd>\n";
+}
+}
+
+sub basicEscape{
+my $t=$_[0];
+$t=~ s/&/&amp;/;
+$t=~ s/</&lt;/;
+$t=~ s/>/&gt;/;
+$t=~ s/"/&quot;/;
+$t=~ s/'/&apos;/;
+return $t;
+}
